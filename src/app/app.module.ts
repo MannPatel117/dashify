1f769f55530f30d41e608f489d8258ff90f77f49
MannWinjit <mann.patel@winjit.com>
2023-08-02 12:47:14 +0530

    boiler-plate-code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './pages/main/main.component';
+import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    LoginComponent,
+    HomeComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
